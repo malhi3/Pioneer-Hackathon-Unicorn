48f37832ad2689d290e6d73786ca3bdf5a20d2cc
siddharthsrivastava0501 <siddharthsrivastava76@gmail.com>
2020-04-12 13:33:26 +0800

    Almost done with scrapper, just need to add it all to the csv file.

--- scrapper.py
+++ scrapper.py
@@ -2,3 +2,53 @@ import pandas as pd
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+
+#db = pd.read_csv('recipe.csv')
+
+def getBSobject(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+    except AttributeError as e:
+        return None
+    return bs
+
+
+url1 = "https://www.allrecipes.com/search/results/?wt=&sort=re&page="
+
+for i in range(1,2):
+    url = url1 + str(i)
+    soup = getBSobject(url)
+
+    #Need to extract text
+    tempTitles = soup.find_all("span", class_="fixed-recipe-card__title-link")
+
+    #Need to extract URL
+    tempLinks = soup.find_all("h3" ,class_="fixed-recipe-card__h3")
+
+    ingredientList = []
+
+
+    for i in range(0,len(tempLinks)):
+
+        title = tempTitles[i].get_text()
+        url = tempLinks[i].find('a').get('href')
+        print(title)
+        print(url)
+
+        soup = getBSobject(url)
+        ingredients = soup.find_all("span", class_="recipe-ingred_txt added")
+
+        temp = ""
+
+        for i in range(len(ingredients)):
+            concatenate = ingredients[i].get_text().replace(",", ";")
+            temp = temp + ", " + (concatenate)
+
+        temp = temp[2:]
+        print(temp)
+        ingredientList.append(temp)
+        print("-----------------------")
