f8643e53c87aa56cf10a1efc97c79188e68e41dd
siddharthsrivastava0501 <siddharthsrivastava76@gmail.com>
2020-04-12 16:06:35 +0800

    Finished scraping for 980 recipes

--- scrapper.py
+++ scrapper.py
@@ -21,7 +21,8 @@ url1 = "https://www.allrecipes.com/search/results/?wt=&sort=re&page="
 with open('recipe.csv', 'w', newline='') as file:
     writer = csv.writer(file)
 
-    for i in range(1,2):
+    for i in range(1,50):
+        print(i)
         url = url1 + str(i)
         soup = getBSobject(url)
 
@@ -32,22 +33,42 @@ with open('recipe.csv', 'w', newline='') as file:
         tempLinks = soup.find_all("h3" ,class_="fixed-recipe-card__h3")
 
         for i in range(0,len(tempLinks)):
-
             title = tempTitles[i].get_text()
             url = tempLinks[i].find('a').get('href')
             print(title)
-            print(url)
 
             soup = getBSobject(url)
             ingredients = soup.find_all("span", class_="recipe-ingred_txt added")
 
             temp = ""
 
-            for i in range(len(ingredients)):
-                concatenate = ingredients[i].get_text().replace(",", ";")
-                temp = temp + ", " + (concatenate)
+            try:
+                ingredients[0]
+                for i in range(len(ingredients)):
+                    concatenate = ingredients[i].get_text().replace(",", ";")
+                    temp = temp + ", " + (concatenate)
+            except IndexError as I:
+                ingredients = soup.find_all("span", class_="ingredients-item-name")
+
+                for i in range(len(ingredients)):
+                    concatenate = ingredients[i].get_text().replace(",", ";")
+                    concatenate = " ".join(concatenate.split())
+                    temp = temp + ", " + (concatenate)
+
+
+            """
+            if (ingredients[0].get_text() = ""):
+                for i in range(len(ingredients)):
+                    concatenate = ingredients[i].get_text().replace(",", ";")
+                    temp = temp + ", " + (concatenate)
+            else:
+                ingredients = soup.find_all("span", class_="ingredients-item-name")
+
+                for i in range(len(ingredients)):
+                    concatenate = ingredients[i].get_text().replace(",", ";")
+                    concatenate = " ".join(concatenate.split())
+                    temp = temp + ", " + (concatenate)
+            """
 
             temp = temp[2:]
-
             writer.writerow([title, url, temp])
-            
